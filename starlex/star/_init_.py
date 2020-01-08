6204a50b40ee7e8d20a6f36e16c457fdf4ce571b
Aditya Pegasus <56690786+aditya-sarkar441@users.noreply.github.com>
2020-01-08 13:12:45 +0530

    Add files via upload

---
+++ starlex/star/_init_.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 09:37:04 2020
+
+@author: Ashis PC
+"""
+
+class stellar:
+    #lumin- luminousity,rad-radius,mas-mass,st-surface temperature
+    def __init__(self,lumin,rad,mas,time,st):
+        self.lumin=lumin
+        self.rad=rad
+        self.mas=mas
+        self.time=time
+        self.st=st
+    
+star1=stellar('','','','','')
