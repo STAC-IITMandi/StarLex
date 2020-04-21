a862d959b686b11897b2183587c2a58bb59efd0c
Aditya Pegasus <56690786+aditya-sarkar441@users.noreply.github.com>
2020-04-21 20:43:28 +0530

    Update _init_.py

--- starlex/star/_init_.py
+++ starlex/star/_init_.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  6 09:37:04 2020
-
-@author: Ashis PC
-"""
+from vpython import *
 
 class stellar:
-    #lumin- luminousity,rad-radius,mas-mass,st-surface temperature
-    def __init__(self,lumin,rad,mas,time,st):
+    #lumin- luminousity,rad-radius,mas-mass,st-surface temperature,t-time
+    def __init__(self,lumin,rad,mas,t,st):
         self.lumin=lumin
         self.rad=rad
         self.mas=mas
-        self.time=time
+        self.t=t
         self.st=st
+    def star(self):
+        scene=canvas()
+        scene.autoscale = False
+        sphere(pos=vector(0,0,0),texture="https://i.imgur.com/1nVWbbd.jpg",radius=self.rad,shininess=self.lumin)
+        b = sphere(color=color.white,opacity=100)
+        b
     
-star1=stellar('','','','','')
+star1=stellar(100,25,300,5,300)
+star1.star()
